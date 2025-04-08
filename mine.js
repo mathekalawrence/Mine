868b5bb47ff1c56d4e1dc5750c4daed09df92175
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-04-08 08:38:23 +0300

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Horizon</title>
+
+	<style type="text/css">
+		h2,h3{
+			text-align: center;
+			text
+		}
+		h3{
+
+		}
+
+
+
+	</style>
+</head>
+<body>
+	<h2>HorizoN Group</h2>
+	<h3><i>Unlocking the Impossible...</i></h3>
+
+	<script type="text/javascript">
+		console.log("Here we come..", 675*4000);
+		
+		let names=["Larry", "Muia", "Softect", "Horizon", 78839];
+		let n=names[50];
+		console.log(names);
+		console.log(n);
+		//console.log(50);
+
+		//for(let name of names){
+			//console.log(name);
+
+		let animal={
+			name: "Heri",
+			age: 31,
+			color: "white",
+			eye: "green",
+			others: "not set",
+		};
+		for(let prop in animal){
+			console.log(animal[prop]);
+		}
+
+		let here=Object.keys(animal);
+		console.log(here);
+
+		for (let i=0; i<10; i++){
+			console.log("The values are", i);
+			if(i==5){
+				break;
+			}
+		}
+		
+		function muia(){
+			let m=prompt("My name is: ");
+			let n=prompt("Your profession..");
+			console.log("Greetings, am", m + "..");
+			console.log("I work as a ", n + ".");
+		}("Enter the first value:");
+			let b=prompt("Enter the second value:");
+			console.log("The sum of the two values is", a+b);
+		}
+		addV();
+		//addV(242,12);
+		
+
+		function n(x,y) {
+			console.log(x+y);
+		}
+		n(452,1009);
+		*/
+		console.log(3223*56);
+
+		let d=y=>console.log(y);
+		d("Muia is the real legend!"); //invoking the function
+
+		//arithmetic values
+		threeValues=(x,y,z)=>console.log('The end result is: ', x*y/0);
+		threeValues(123,4000,3);
+
+		//spread Operator
+		let sp=['Klangu','Ndithoo', 'Riverside', 'Sloga..'];
+		console.log(...sp);
+
+		//rest parameter
+	function here(m1, ...m2){
+		console.log(m1, m2);
+	}
+	here("alot", 'may live', 'to be said', 'but', 'very few ever manage to reach the', 'threshold!');
+
+	let mine=prompt("How can you rate yourself so far?")
+	console.log('My ratings range at', mine);
+
+    
+	let arr1=prompt('Enter the value',[]);
+
+	for(let i=0; i<20; i++){
+		let result=(i, 3*i);
+		arr1.push(result)
+	}
+	console.log(arr1);
+	
+
+
+
+
+	</script>
